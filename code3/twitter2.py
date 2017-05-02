4be11ca76867ddf6b16552793baa89bbc1e56ea1
Charles Severance <csev@umich.edu>
2017-05-02 07:25:18 -0400

    Update

--- code3/twitter2.py
+++ code3/twitter2.py
@@ -1,8 +1,17 @@
 import urllib.request, urllib.parse, urllib.error
 import twurl
 import json
+import ssl
+
+# https://apps.twitter.com/
+# Create App and get the four strings, put them in hidden.py
 
 TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 while True:
     print('')
@@ -11,14 +20,19 @@ while True:
     url = twurl.augment(TWITTER_URL,
                         {'screen_name': acct, 'count': '5'})
     print('Retrieving', url)
-    connection = urllib.request.urlopen(url)
+    connection = urllib.request.urlopen(url, context=ctx)
     data = connection.read().decode()
+
+    js = json.loads(data)
+    print(json.dumps(js, indent=2))
+
     headers = dict(connection.getheaders())
     print('Remaining', headers['x-rate-limit-remaining'])
-    js = json.loads(data)
-    print(json.dumps(js, indent=4))
 
     for u in js['users']:
         print(u['screen_name'])
+        if 'status' not in u:
+            print('   * No status found')
+            continue
         s = u['status']['text']
         print('  ', s[:50])
