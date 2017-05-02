1edc17d149e3c373e4add8c9a5fa758fb4ee1552
Charles Severance <csev@umich.edu>
2017-05-02 07:41:13 -0400

    Tweaks

--- book.php
+++ book.php
@@ -2,90 +2,6 @@
 <?php include("nav.php");?>
 <div class="hide-for-large" id="mobile-support"></div>
 
-<div id="first-row" class="row">
-     <div class="small-12 medium-12 large-3 large-push-9 columns">
-        <div class="text-center">
-            <a href="http://amzn.to/29w0lsX" target="_blank">
-                <img width="auto" height="100%" src="cover3-final.jpg" alt="A picture of the book cover art"/>
-            </a>
-        </div>
-    </div>
-
-    <div class="small-12 medium-12 large-9 large-pull-3 columns">
-        <h2>Python for Everybody</h2>
-        <h3>Exploring Data In Python 3</h3>
-        <p>
-            New Edition! The goal of this book is to provide an Informatics-oriented introduction to programming. The primary difference between a computer science approach and the Informatics approach taken in this book is a greater focus on using Python to solve data analysis problems common in the world of Informatics.
-The <a href="#python-for-informatics">Python 2 version</a> of the book is still available.
-There are multiple translations of the Python 2 book - the Python 3 version
-of the book has not been translated.
-        </p>
-        <p>
-            Updated to use Python 3, the book is now available in a variety of formats:
-        </p>
-
-
-<!-- new book accordion menu -->
-<div class="acc-menu">
-<ul class="vertical menu" data-accordion-menu>
-  <li>
-     <a href="#">English</a>
-     <ul class="menu vertical nested is-active">
-       <li class="menu-text">Printed Book on <a href="http://amzn.to/29w0lsX" target="_blank">Amazon</a></li>
-       <li class="menu-text"><a href="http://amzn.to/29LrtC0" target="_blank">Kindle edition</a> of the book</li>
-       <li class="menu-text"><a href="https://books.trinket.io/pfe/index.html" target="_blank">Interactive HTML on Trinket.io</a></li>
-       <li class="menu-text"><a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.pdf" target="_blank">Download PDF</a></li>
-       <li class="menu-text"><a href="http://do1.dr-chuck.com/pythonlearn/EN_us/pythonlearn.epub" target="_blank">Download EPUB</a></li>
-       <li class="menu-text"><a href="https://github.com/csev/pythonlearn/raw/master/book3/zips/pfe.zip" target="_blank">Download HTML in a ZIP (~6MB)</a></li>
-     </ul>
-  </li>
-  <li>
-     <a href="#">Other Languages</a>
-     <ul class="menu vertical nested">
-        <li class="menu-text">
-There are multiple translations of the 
-<a href="#python-for-informatics">Python 2 version</a> 
-of this book - the Python 3 version
-of the book has not yet been translated.
-If you are interested in translating the Python 3 book, please contact
-the author.
-</li>
-     </ul>
-  </li>
-</ul>
-</div>
-<!-- end new book accordion menu -->
-</div>
-</div>
-<div class="row">
-    <div class="small-12 columns">
-        <br>
-        <p>
-            The sample code and data files for the book is here: <a href="http://www.pythonlearn.com/code3/" target="_blank">Code Samples</a>.
-        </p>
-        <p>
-            Chapters 2-10 are heavily adapted from the open book titled: "<a href="http://www.greenteapress.com/thinkpython/thinkCSpy/" target="_blank">Think Python: How to Think like a Computer Scientist</a>" by <a href="http://allendowney.com/" target="_blank">Allen B. Downey</a>.
-        </p>
-        <p>
-            All of these materials are free and I want you to take them, use them and reuse them. If you would like to leave a tip, feel free to do so using the PayPal Donate button below.
-        </p>
-<center>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHNwYJKoZIhvcNAQcEoIIHKDCCByQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBErqbe3o+ku/VcLZjRolYQZ34eki752zTJItw3o8TpXq+MpBdKjiZlbUnQk5rpKDQNgVk7LjPH2yvJu72ZIfQ0/pDeckPIdbFDlxzwzhctFuiG4mFALqgLyBYGOp03dcAS5su0FApKIXV4D9wrNsBaahN3KLllFMGzRrgQtx4HSDELMAkGBSsOAwIaBQAwgbQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQI5Nm5JTS96LqAgZDEoepR053KEEOcdxVoa6ZkP6O9Hg3gJO1YvBlOV118sfd7NFg5dLb2d8Rb8UaKfjwS5ZEFl6jZNfyywBsA/qTPgtwEcFAwHgDnrOskU8CVUMz6SGL5cEUEP3Dp8dNEGjXO/TvEugGpjuwFeovvgwAPpPmEjeXbZzbJ+WSZQHFOgS0g0WuoWQTIzTe+hcQwfU2gggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xNTA1MTcxMzI2MTRaMCMGCSqGSIb3DQEJBDEWBBS+5hvvdGO+qAz/7b6/4oDHL8HE/DANBgkqhkiG9w0BAQEFAASBgLDBx/CFZabTxV4grO/QM1ny9Mh1+9hIAKhz1tRGNvUfASZcbRD+TVYbprEpaQTVTX+GzkJ6UjXlXZBBOMHUWju/K8frzamYsx3lDupXSYhSyQISDk2JO8zWebgJV08aT478hKoC4o3gbJ2A8wqKMI5LVZPCXdn0DRTSqzESMVZ4-----END PKCS7-----
-">
-<div class="hide-for-large">
-    <input type="image" width="60%" height="auto" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" name="submit" alt="PayPal - The safer, easier way to pay online!">
-</div>
-<div class="show-for-large">
-    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" name="submit" alt="PayPal - The safer, easier way to pay online!">
-</div>
-<img alt=""src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif">
-</form>
-</center>
-</div>
-</div>
-<hr/>
 <div class="row">
      <div class="small-12 medium-12 large-3 large-push-9 columns">
         <div class="text-center">
@@ -99,7 +15,7 @@ the author.
         <h2>Python for Informatics</h2>
         <h3>Exploring Information</h3>
         <p>
-            A previous version of the book, utilizing Python 2.
+            You may be interested in the Python 3 version of this book, available at <a href="http://www.py4e.com/book">http://www.py4e.com/book</a>.
         </p>
         <p>
             The goal of this book is to provide an Informatics-oriented introduction to programming. The primary difference between a computer science approach and the Informatics approach taken in this book is a greater focus on using Python to solve data analysis problems common in the world of Informatics.
